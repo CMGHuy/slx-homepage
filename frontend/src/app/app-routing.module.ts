629f9c172e94d0ddd21a85997dabfc429a4a5f89
Huy Cao <caominhgiahuy@gmail.com>
2023-02-05 17:17:21 +0100

    define basic components. Closes #4

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './blog/blog.component';
+import { CareerComponent } from './career/career.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductsComponent } from './products/products.component';
+import { SupplyComponent } from './supply/supply.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomepageComponent },
+  { path: 'blog', component: BlogComponent },
+  { path: 'career', component: CareerComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'services', component: SupplyComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: HomepageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductsComponent } from './products/products.component';
+import { CareerComponent } from './career/career.component';
+import { CompanyComponent } from './company/company.component';
+import { BlogComponent } from './blog/blog.component';
+import { SupplyComponent } from './supply/supply.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ProductsComponent,
+    CareerComponent,
+    CompanyComponent,
+    BlogComponent,
+    SupplyComponent
   ],
   imports: [
     BrowserModule,
