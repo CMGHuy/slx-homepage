19a08a2192b43d6f612708589d65756bcdaa2ef7
Huy Cao <caominhgiahuy@gmail.com>
2023-02-05 16:15:44 +0100

    create basic components. Closes #1

---
+++ backend/src/test/java/ch/solenix/homepage/HomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.solenix.homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
